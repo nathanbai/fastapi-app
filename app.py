62a1f47727566ecbfcc10e4a82e1f06adb215a51
Yicheng Bai <baiyicheng@gmail.com>
2026-06-10 10:48:28 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI in Docker!"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
